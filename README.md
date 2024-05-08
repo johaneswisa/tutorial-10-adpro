f908cdc3272278ccc77c6be99333155bc6f7d520
Johan <johaneswisanggeni2003@gmail.com>
2024-05-08 19:49:03 +0700

    Experiment 1.2: Understanding how it works.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Refleksi
+
+
+![alt text](<Screenshot (565).png>)
+hey hey dicetak terlebih dahulu. code untuk hey hey sebenarnya berada di bawah howdy dan done. tetapi hal ini tetap terjadi
+karena code yang mencetak howdy dan done berjalan secara asinkronus. maka dari itu hey hey bisa dicetak terlebih dahulu.
+
+
--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Johan's computer: done!");
     });
-
+    println!("Johan's computer: hey hey!");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
